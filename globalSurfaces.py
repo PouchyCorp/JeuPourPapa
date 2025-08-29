13dde867c6fea077f7bb1cc4e154090ae0914e78
Paul Baumard <92679451+PouchyCorp@users.noreply.github.com>
2025-08-29 22:35:38 +0200

    ob

--- globalSurfaces.py
+++ globalSurfaces.py
@@ -5,9 +5,9 @@ import pygame as pg
 
 
 LEVELS_SPRITES = [
-    sprite.load_image("assets/photos/4.png", size=(1100, 1100)),
-    sprite.load_image("assets/photos/2.png", size=(1100, 1100)),
-    sprite.load_image("assets/photos/2017.png", size=(1100, 1100))
+    sprite.load_image("assets/photos/4.png"),
+    sprite.load_image("assets/photos/2.png"),
+    sprite.load_image("assets/photos/2017.jpg")
 ]
 
 PUZZLE_PIECE = sprite.load_image("assets/images/puzzlepiece.png", 1.1)
