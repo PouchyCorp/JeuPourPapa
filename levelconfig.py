af6e92ad34ccd7dad76262b37d16e4b738d23297
femboyv <baumard.louane@gmail.com>
2025-08-29 23:59:00 +0200

    added questions

--- levelconfig.py
+++ levelconfig.py
@@ -16,8 +16,8 @@ LEVEL_1 = LevelConfig(
         minigame.Quiz("Choup !", ["Choup !", "Kipik."], question="Choupchoup ?"),
         minigame.Memory((4, 4), gs.MEMORY_CARDS_1),
         minigame.Quiz(
-            "Rasinari",
-            ["Rasinari", "poplaca"],
+            "Rășinari",
+            ["Rășinari", "Poplaca", "Păltiniș"],
             caption_image=gs.RASINARI_PHOTO,
             question="Où a été prise cette photo ?",
         ),
@@ -38,9 +38,16 @@ LEVEL_2 = LevelConfig(
 
 LEVEL_3 = LevelConfig(
     minigames=[
-        minigame.Quiz("Answer 9", ["Answer 9", "Answer 10"]),
+        minigame.Quiz(
+            "2011",
+            ["2011", "2013", "2016"],
+            caption_image=gs.PHOTO_OF_2011,
+            question="Quand a été prise cette photo ?",
+        ),
         minigame.ColorSequenceMemory(13),
-        minigame.Quiz("Answer 11", ["Answer 11", "Answer 12"]),
+        minigame.Quiz(
+            "Louane", ["Louane", "Isabelle"], question="Qui n'aime pas les poireaux?"
+        ),
         minigame.SlidingPuzzle((3, 3), gs.LEVELS_SPRITES[0]),
     ],
     background=gs.LEVELS_SPRITES[2],
